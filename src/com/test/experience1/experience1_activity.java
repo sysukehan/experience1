9250ad02a76ebdec38782437037fc6d228d3d0df
kehan <642892830@qq.com>
2016-03-04 11:46:35 +0800

    experience1 first commit

---
+++ src/com/test/experience1/experience1_activity.java
@@ -0,0 +1,14 @@
+package com.test.experience1;
+
+import com.example.experience1.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class experience1_activity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
